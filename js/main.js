fd3745ee344660c945fb72a8de7d0eee6f6d1c42
Zenrac <zenrac@outlook.fr>
2020-03-20 18:03:55 +0100

    End of Watora

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
   // Remove url text on mouseover for icons links
-  $('.icon a').each(function(){
-    $(this).attr('onclick','window.location.href="' + $(this).attr('href')+'"');
+  $('.icon a').each(function() {
+    $(this).attr('onclick', 'window.location.href="' + $(this).attr('href') + '"');
     $(this).removeAttr('href');
   });
 });
@@ -12,32 +12,31 @@ jQuery(document).ready(function($) {
    * Set footer always on the bottom of the page
    */
   function footerAlwayInBottom(footerSelector) {
-      var docHeight = $(window).height();
-      var footerTop = footerSelector.position().top + footerSelector.height();
-      if (footerTop < docHeight) {
-          footerSelector.css("margin-top", (docHeight - footerTop) + "px");
-      }
-      else {
-        footerSelector.css('margin-top', '0px');
-      }
+    var docHeight = $(window).height();
+    var footerTop = footerSelector.position().top + footerSelector.height();
+    if (footerTop < docHeight) {
+      footerSelector.css("margin-top", (docHeight - footerTop) + "px");
+    } else {
+      footerSelector.css('margin-top', '0px');
+    }
   }
   // Apply when page is loading
   footerAlwayInBottom($("#footer"));
 
   // Apply when page is fully loaded
   $(window).on("load", function() {
-      footerAlwayInBottom($("#footer"));
-      $(window).trigger('resize');
+    footerAlwayInBottom($("#footer"));
+    $(window).trigger('resize');
   });
 
   // Apply when page is resizing
   $(window).resize(function() {
-      footerAlwayInBottom($("#footer"));
+    footerAlwayInBottom($("#footer"));
   });
 
   // Apply every 25 ms
-  window.setInterval(function(){
-      footerAlwayInBottom($("#footer"));
+  window.setInterval(function() {
+    footerAlwayInBottom($("#footer"));
   }, 25);
 
 });
